29b3957e74938cdb06ab61a2c9df2cfd72014209
jainChetan81 <jain.cj.chetan@gmail.com>
2021-01-21 14:49:52 +0530

    changes for heroku

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { Suspense } from "react";
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
+    <BrowserRouter basename="/">
+        <Suspense
+            fallback={
+                <div className="text-center m-5 text-uppercase">Loading...</div>
+            }>
+            <App />
+        </Suspense>
     </BrowserRouter>,
     document.getElementById("root")
 );
